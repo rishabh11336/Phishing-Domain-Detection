1227c0332178d99fe0c3bead0c7217b47a4fc04a
rishabh11336 <rishabh11336@gmail.com>
2024-04-01 19:02:28 +0530

    Add .gitignore file and update README.md and HLD document

---
+++ url_parser.py
@@ -0,0 +1,381 @@
+# Table 1. Dataset attributes based on URL.
+
+# Nr.	Attribute	Format	Description	Values
+# 1	qty_dot_url	Number of ”.” signs	Numeric
+# 2	qty_hyphen_url	Number of ”-” signs	Numeric
+# 3	qty_underline_url	Number of ”_” signs	Numeric
+# 4	qty_slash_url	Number of ”/” signs	Numeric
+# 5	qty_questionmark_url	Number of ”?” signs	Numeric
+# 6	qty_equal_url	Number of ”=” sings	Numeric
+# 7	qty_at_url	Number of ”@” signs	Numeric
+# 8	qty_and_url	Number of ”&” signs	Numeric
+# 9	qty_exclamation_url	Number of ”!” signs	Numeric
+# 10	qty_space_url	Number of ” ” signs	Numeric
+# 11	qty_tilde_url	Number of signs	Numeric
+# 12	qty_comma_url	Number of ”,” signs	Numeric
+# 13	qty_plus_url	Number of ”+” signs	Numeric
+# 14	qty_asterisk_url	Number of ”*” signs	Numeric
+# 15	qty_hashtag_url	Number of ”#” signs	Numeric
+# 16	qty_dollar_url	Number of ”$” signs	Numeric
+# 17	qty_percent_url	Number of ”%” signs	Numeric
+# 18	qty_tld_url	Top level domain character length	Numeric
+# 19	length_url	Number of characters	Numeric
+# 20	email_in_url	Is email present	Boolean	[0, 1]
+
+# Table 2. Dataset attributes based on domain URL.
+
+# Nr.	Attribute	Format	Description	Values
+# 1	qty_dot_domain	Number of ”.” signs	Numeric
+# 2	qty_hyphen_domain	Number of ”-” signs	Numeric
+# 3	qty_underline_domain	Number of ”_” signs	Numeric
+# 4	qty_slash_domain	Number of ”/” signs	Numeric
+# 5	qty_questionmark_domain	Number of ”?” signs	Numeric
+# 6	qty_equal_domain	Number of ”=” signs	Numeric
+# 7	qty_at_domain	Number of ”@” signs	Numeric
+# 8	qty_and_domain	Number of ”&” signs	Numeric
+# 9	qty_exclamation_domain	Number of ”!” signs	Numeric
+# 10	qty_space_domain	Number of ” ” signs	Numeric
+# 11	qty_tilde_domain	Number of ”signs	Numeric
+# 12	qty_comma_domain	Number of ”,” signs	Numeric
+# 13	qty_plus_domain	Number of ”+” signs	Numeric
+# 14	qty_asterisk_domain	Number of ”*” signs	Numeric
+# 15	qty_hashtag_domain	Number of ”#” signs	Numeric
+# 16	qty_dollar_domain	Number of ”$” signs	Numeric
+# 17	qty_percent_domain	Number of ”%” signs	Numeric
+# 18	qty_vowels_domain	Number of vowels	Numeric
+# 19	domain_length	Number of domain characters	Numeric
+# 20	domain_in_ip	URL domain in IP address format	Boolean	[0, 1]
+# 21	server_client_domain	”server” or ”client” in domain	Boolean	[0, 1]
+
+# Table 3. Dataset attributes based on URL directory.
+
+# Nr.	Attribute	Format	Description	Values
+# 1	qty_dot_directory	Number of ”.” signs	Numeric
+# 2	qty_hyphen_directory	Number of ”-” signs	Numeric
+# 3	qty_underline_directory	Number of ”_” signs	Numeric
+# 4	qty_slash_directory	Number of ”/” signs	Numeric
+# 5	qty_questionmark_directory	Number of ”?” signs	Numeric
+# 6	qty_equal_directory	Number of ”=” signs	Numeric
+# 7	qty_at_directory	Number of ”@” signs	Numeric
+# 8	qty_and_directory	Number of ”&” signs	Numeric
+# 9	qty_exclamation_directory	Number of ”!” signs	Numeric
+# 10	qty_space_directory	Number of ” ” signs	Numeric
+# 11	qty_tilde_directory	Number of ”signs	Numeric
+# 12	qty_comma_directory	Number of ”,” signs	Numeric
+# 13	qty_plus_directory	Number of ”+” signs	Numeric
+# 14	qty_asterisk_directory	Number of ”*” signs	Numeric
+# 15	qty_hashtag_directory	Number of ”#” signs	Numeric
+# 16	qty_dollar_directory	Number of ”$” signs	Numeric
+# 17	qty_percent_directory	Number of ”%” signs	Numeric
+# 18	directory_length	Number of directory characters	Numeric
+
+# Table 4. Dataset attributes based on URL file name.
+
+# Nr.	Attribute	Format	Description	Values
+# 1	qty_dot_file	Number of ”.” signs	Numeric
+# 2	qty_hyphen_file	Number of ”-” signs	Numeric
+# 3	qty_underline_file	Number of ”_” signs	Numeric
+# 4	qty_slash_file	Number of ”/” signs	Numeric
+# 5	qty_questionmark_file	Number of ”?” signs	Numeric
+# 6	qty_equal_file	Number of ”=” signs	Numeric
+# 7	qty_at_file	Number of ”@” signs	Numeric
+# 8	qty_and_file	Number of ”&” signs	Numeric
+# 9	qty_exclamation_file	Number of ”!” signs	Numeric
+# 10	qty_space_file	Number of ” ” signs	Numeric
+# 11	qty_tilde_file	Number of ”signs	Numeric
+# 12	qty_comma_file	Number of ”,” signs	Numeric
+# 13	qty_plus_file	Number of ”+” signs	Numeric
+# 14	qty_asterisk_file	Number of ”*” signs	Numeric
+# 15	qty_hashtag_file	Number of ”#” signs	Numeric
+# 16	qty_dollar_file	Number of ”$” signs	Numeric
+# 17	qty_percent_file	Number of ”%” signs	Numeric
+# 18	file_length	Number of file name characters	Numeric
+
+
+# Table 5. Dataset attributes based on URL parameters.
+
+# Nr.	Attribute	Format	Description	Values
+# 1	qty_dot_params	Number of ”.” signs	Numeric
+# 2	qty_hyphen_params	Number of ”-” signs	Numeric
+# 3	qty_underline_params	Number of ”_” signs	Numeric
+# 4	qty_slash_params	Number of ”/” signs	Numeric
+# 5	qty_questionmark_params	Number of ”?” signs	Numeric
+# 6	qty_equal_params	Number of ”=” signs	Numeric
+# 7	qty_at_params	Number of ”@” signs	Numeric
+# 8	qty_and_params	Number of ”&” signs	Numeric
+# 9	qty_exclamation_params	Number of ”!” signs	Numeric
+# 10	qty_space_params	Number of ” ” signs	Numeric
+# 11	qty_tilde_params	Number of ”signs	Numeric
+# 12	qty_comma_params	Number of ”,” signs	Numeric
+# 13	qty_plus_params	Number of ”+” signs	Numeric
+# 14	qty_asterisk_params	Number of ”*” signs	Numeric
+# 15	qty_hashtag_params	Number of ”#” signs	Numeric
+# 16	qty_dollar_params	Number of ”$” signs	Numeric
+# 17	qty_percent_params	Number of ”%” signs	Numeric
+# 18	params_length	Number of parameters characters	Numeric
+# 19	tld_present_params	TLD present in parameters	Boolean	[0, 1]
+# 20	qty_params	Number of parameters	Numeric
+
+# Table 6. Dataset attributes based on resolving URL and external services.
+
+# Nr.	Attribute	Format	Description	Values
+# 1	time_response	Domain lookup time response	Numeric
+# 2	domain_spf	Domain has SPF Boolean	[0, 1]
+# 3	asn_ip	ASN Numeric
+# 4	time_domain_activation	Domain activation time (in days)	Numeric
+# 5	time_domain_expiration	Domain expiration time (in days)	Numeric
+# 6	qty_ip_resolved	Number of resolved IPs	Numeric
+# 8	qty_nameservers	Number of resolved NS Numeric
+# 9	qty_mx_servers	Number of MX servers	Numeric
+# 10	ttl_hostname	Time-To-Live (TTL)	Numeric
+# 11	tls_ssl_certificate	Has valid TLS/SSL certificate	Boolean	[0, 1]
+# 12	qty_redirects	Number of redirects	Numeric
+# 13	url_google_index	Is URL indexed on Google	Boolean	[0, 1]
+# 14	domain_google_index	Is domain indexed on Google	Boolean	[0, 1]
+# 15	url_shortened	Is URL shortened	Boolean
+
+# write down a py script that break down a given url into 111 components listed above.
+# give me single object containing all the 111 components. Store 0 for missing values.
+# store 0 and 1 for boolean values.
+
+
+from urllib.parse import urlparse
+import re
+import whois
+import datetime
+import requests
+import socket
+import ssl
+
+
+class URLParser:
+    def __init__(self, url):
+        self.url = url
+        self.url_components = urlparse(url)
+        self.domain = self.url_components.netloc
+        self.directory = self.url_components.path
+        self.file = self.url_components.path.split("/")[-1]
+        self.parameters = self.url_components.query
+        self.components = {}
+        self.components.update(self.get_domain_components())
+        self.components.update(self.get_directory_components())
+        self.components.update(self.get_file_components())
+        self.components.update(self.get_parameters_components())
+        self.components.update(self.get_resolving_components())
+        self.components.update(self.get_external_services_components())
+        self.components.update(self.get_url_components())
+
+    def get_domain_components(self):
+
+        domain_components = {
+            "qty_dot_domain": self.domain.count("."),
+            "qty_hyphen_domain": self.domain.count("-"),
+            "qty_underline_domain": self.domain.count("_"),
+            "qty_slash_domain": self.domain.count("/"),
+            "qty_questionmark_domain": self.domain.count("?"),
+            "qty_equal_domain": self.domain.count("="),
+            "qty_at_domain": self.domain.count("@"),
+            "qty_and_domain": self.domain.count("&"),
+            "qty_exclamation_domain": self.domain.count("!"),
+            "qty_space_domain": self.domain.count(" "),
+            "qty_tilde_domain": self.domain.count("~"),
+            "qty_comma_domain": self.domain.count(","),
+            "qty_plus_domain": self.domain.count("+"),
+            "qty_asterisk_domain": self.domain.count("*"),
+            "qty_hashtag_domain": self.domain.count("#"),
+            "qty_dollar_domain": self.domain.count("$"),
+            "qty_percent_domain": self.domain.count("%"),
+            "qty_vowels_domain": sum(
+                [self.domain.count(vowel) for vowel in "aeiouAEIOU"]
+            ),
+            "domain_length": len(self.domain),
+            "domain_in_ip": 1 if self.domain.replace(".", "").isdigit() else 0,
+            "server_client_domain": (
+                1
+                if "server" in self.domain.lower() or "client" in self.domain.lower()
+                else 0
+            ),
+        }
+        return domain_components
+
+    def get_directory_components(self):
+        directory_components = {
+            "qty_dot_directory": self.directory.count("."),
+            "qty_hyphen_directory": self.directory.count("-"),
+            "qty_underline_directory": self.directory.count("_"),
+            "qty_slash_directory": self.directory.count("/"),
+            "qty_questionmark_directory": self.directory.count("?"),
+            "qty_equal_directory": self.directory.count("="),
+            "qty_at_directory": self.directory.count("@"),
+            "qty_and_directory": self.directory.count("&"),
+            "qty_exclamation_directory": self.directory.count("!"),
+            "qty_space_directory": self.directory.count(" "),
+            "qty_tilde_directory": self.directory.count("~"),
+            "qty_comma_directory": self.directory.count(","),
+            "qty_plus_directory": self.directory.count("+"),
+            "qty_asterisk_directory": self.directory.count("*"),
+            "qty_hashtag_directory": self.directory.count("#"),
+            "qty_dollar_directory": self.directory.count("$"),
+            "qty_percent_directory": self.directory.count("%"),
+            "directory_length": len(self.directory),
+        }
+        return directory_components
+
+    def get_file_components(self):
+        file_components = {
+            "qty_dot_file": self.file.count("."),
+            "qty_hyphen_file": self.file.count("-"),
+            "qty_underline_file": self.file.count("_"),
+            "qty_slash_file": self.file.count("/"),
+            "qty_questionmark_file": self.file.count("?"),
+            "qty_equal_file": self.file.count("="),
+            "qty_at_file": self.file.count("@"),
+            "qty_and_file": self.file.count("&"),
+            "qty_exclamation_file": self.file.count("!"),
+            "qty_space_file": self.file.count(" "),
+            "qty_tilde_file": self.file.count("~"),
+            "qty_comma_file": self.file.count(","),
+            "qty_plus_file": self.file.count("+"),
+            "qty_asterisk_file": self.file.count("*"),
+            "qty_hashtag_file": self.file.count("#"),
+            "qty_dollar_file": self.file.count("$"),
+            "qty_percent_file": self.file.count("%"),
+            "file_length": len(self.file),
+        }
+        return file_components
+
+    def get_parameters_components(self):
+        parameters_components = {
+            "qty_dot_params": self.parameters.count("."),
+            "qty_hyphen_params": self.parameters.count("-"),
+            "qty_underline_params": self.parameters.count("_"),
+            "qty_slash_params": self.parameters.count("/"),
+            "qty_questionmark_params": self.parameters.count("?"),
+            "qty_equal_params": self.parameters.count("="),
+            "qty_at_params": self.parameters.count("@"),
+            "qty_and_params": self.parameters.count("&"),
+            "qty_exclamation_params": self.parameters.count("!"),
+            "qty_space_params": self.parameters.count(" "),
+            "qty_tilde_params": self.parameters.count("~"),
+            "qty_comma_params": self.parameters.count(","),
+            "qty_plus_params": self.parameters.count("+"),
+            "qty_asterisk_params": self.parameters.count("*"),
+            "qty_hashtag_params": self.parameters.count("#"),
+            "qty_dollar_params": self.parameters.count("$"),
+            "qty_percent_params": self.parameters.count("%"),
+            "params_length": len(self.parameters),
+            "tld_present_params": (
+                1
+                if self.parameters.endswith(".com")
+                or self.parameters.endswith(".org")
+                or self.parameters.endswith(".net")
+                else 0
+            ),
+            "qty_params": len(self.parameters.split("&")),
+        }
+        return parameters_components
+
+    def get_resolving_components(self):
+        try:
+            ip = socket.gethostbyname(self.domain)
+            domain = whois.whois(self.domain)
+            resolving_components = {
+                "time_response": domain.creation_date - domain.updated_date,
+                "domain_spf": 1 if "spf" in domain.text.lower() else 0,
+                "asn_ip": ip,
+                "time_domain_activation": (
+                    datetime.datetime.now() - domain.creation_date
+                ).days,
+                "time_domain_expiration": (
+                    domain.expiration_date - datetime.datetime.now()
+                ).days,
+                "qty_ip_resolved": len(socket.gethostbyname_ex(self.domain)),
+                "qty_nameservers": len(domain.name_servers),
+                "qty_mx_servers": len(domain.get_mx()),
+                "ttl_hostname": socket.gethostbyname_ex(self.domain)[-1],
+                "tls_ssl_certificate": (
+                    1 if ssl.get_server_certificate((self.domain, 443)) else 0
+                ),
+                "qty_redirects": len(requests.get(self.url).history),
+                "url_google_index": (
+                    1
+                    if requests.get(f"https://www.google.com/search?q={self.url}").text
+                    else 0
+                ),
+                "domain_google_index": (
+                    1
+                    if requests.get(
+                        f"https://www.google.com/search?q={self.domain}"
+                    ).text
+                    else 0
+                ),
+                "url_shortened": 1 if len(self.url) < 20 else 0,
+            }
+            return resolving_components
+        except:
+            return {
+                key: 0
+                for key in [
+                    "time_response",
+                    "domain_spf",
+                    "asn_ip",
+                    "time_domain_activation",
+                    "time_domain_expiration",
+                    "qty_ip_resolved",
+                    "qty_nameservers",
+                    "qty_mx_servers",
+                    "ttl_hostname",
+                    "tls_ssl_certificate",
+                    "qty_redirects",
+                    "url_google_index",
+                    "domain_google_index",
+                    "url_shortened",
+                ]
+            }
+
+    def get_external_services_components(self):
+        return {}
+
+    def get_url_components(self):
+        return {
+            "qty_dot_url": self.url.count("."),
+            "qty_hyphen_url": self.url.count("-"),
+            "qty_underline_url": self.url.count("_"),
+            "qty_slash_url": self.url.count("/"),
+            "qty_questionmark_url": self.url.count("?"),
+            "qty_equal_url": self.url.count("="),
+            "qty_at_url": self.url.count("@"),
+            "qty_and_url": self.url.count("&"),
+            "qty_exclamation_url": self.url.count("!"),
+            "qty_space_url": self.url.count(" "),
+            "qty_tilde_url": self.url.count("~"),
+            "qty_comma_url": self.url.count(","),
+            "qty_plus_url": self.url.count("+"),
+            "qty_asterisk_url": self.url.count("*"),
+            "qty_hashtag_url": self.url.count("#"),
+            "qty_dollar_url": self.url.count("$"),
+            "qty_percent_url": self.url.count("%"),
+            "qty_tld_url": len(self.url_components.netloc.split(".")[-1]),
+            "length_url": len(self.url),
+            "email_in_url": (
+                1 if re.search(r"[a-zA-Z0-9]+@[a-zA-Z]+\.[a-zA-Z]+", self.url) else 0
+            ),
+        }
+
+    def get_all_components(self):
+        return self.components
+
+    def get_all_components_values(self):
+        return list(self.components.values())
+
+    def get_all_components_keys(self):
+        return list(self.components.keys())
+
+
+url = "https://www.google.com/search?q=python+url+parser+example"
+parser = URLParser(url)
+print("URL:", url)
+print("Components:", parser.get_all_components())
+print("Components Values:", parser.get_all_components_values())
+print("Components Keys:", parser.get_all_components_keys())
+print("Components Length:", len(parser.get_all_components_values()))
